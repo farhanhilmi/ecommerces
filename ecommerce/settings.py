cad8e7a99f0e72d3fb220fdabf1a3f2aaa329734
farhanhilmi <farhanhilmi077@gmail.com>
2020-07-27 19:28:12 +0700

    d

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,7 +131,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images-products/'
 
